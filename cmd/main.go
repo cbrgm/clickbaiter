0773a36f836d87721f3c73e15b96ab73801bdafd
Christian Bargmann <chris@cbrgm.net>
2021-03-24 18:22:03 +0100

    feat: add cmd binary to create clickbait headlines from command line

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,25 @@
-package cmd
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/cbrgm/clickbaiter/clickbaiter"
+	"math/rand"
+	"time"
+)
+
+var hashtagsFlag = flag.Bool("hashtags", false, "use hashtags")
+
+func main() {
+	flag.Parse()
+	rand.Seed(time.Now().Unix())
+
+	b := *hashtagsFlag
+
+	fmt.Println(b)
+
+	cbg := clickbaiter.NewGenerator()
+	cbg.UseHashtags(b)
+
+	fmt.Println(cbg.RandomSentence())
+}
